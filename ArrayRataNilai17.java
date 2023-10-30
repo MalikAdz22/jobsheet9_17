88b100da233f3037fe7ac63fffb8aadb29f9ea4e
MalikAdz22 <adzanomalik@gmail.com>
2023-10-29 22:18:45 -0800

    ratanilai

--- ArrayRataNilai17.java
+++ ArrayRataNilai17.java
@@ -4,19 +4,29 @@ public class ArrayRataNilai17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int[] nilaiMhs = new int[sc.nextInt()];
+        double totalLulus = 0, totalTdkLulus = 0, rataLls, rataTdkLls; 
+        int lulus = 0, tdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++){
-            System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70){
+                lulus += 1;
+                totalLulus += nilaiMhs[i];
+            } else {
+                tdkLulus += 1;
+                totalTdkLulus += nilaiMhs[i];
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+
+        rataLls = totalLulus / lulus;
+        rataTdkLls = totalTdkLulus / tdkLulus; 
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + lulus);
+        System.out.println("Rata - rata nilai lulus = " + rataLls);
+        System.out.println("Rata - rata nilai tidak lulus = " + rataTdkLls);
     }
 
 }
