c7f02192bf9a5427c6f090f5daa6108a1967bca9
MalikAdz22 <adzanomalik@gmail.com>
2023-10-29 19:11:31 -0800

    Arraybilangan

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class ArrayNilai17 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc17 = new Scanner(System.in);
 
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
 
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70){
+
+            }
             System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
         }
     }
---
+++ ArrayRataNilai17.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai17 {
+    public static void main(String[] args) {
+        Scanner input
+    }
+
+}
